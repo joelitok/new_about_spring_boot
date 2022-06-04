708b5847aa32322f93cfb4b615be28a089587bcd
joelitok <joelnkouatchet@gmail.com>
2022-06-04 18:34:13 +0200

    add new information

--- src/main/java/com/securnew/sercurenew/domain/service/UserServiceImpl.java
+++ src/main/java/com/securnew/sercurenew/domain/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.securnew.sercurenew.domain.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -25,9 +28,22 @@ public class UserServiceImpl  implements UserService, UserDetailsService{
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        AppUser user = appUserRepo.findByUsername(username);
+        if(user==null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }else{
+            log.info("User found in the database: {}", username);
+        }
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role->{
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), authorities);
     }
-    
+
 
     @Override
     public AppUser saveUser(AppUser appUser) {
